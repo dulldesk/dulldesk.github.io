1035d741d162c045b1dc38c16f8c0af44faf6670
dulldesk <54601393+dulldesk@users.noreply.github.com>
2020-07-17 16:39:32 -0400

    Move nav and footer to subdirectory
    For organizational purposes

--- js/base.js
+++ js/base.js
@@ -3,7 +3,7 @@ $(document).ready(() => {
 
 		$("body").prepend($("<nav id='navbar'></nav>") )
 	})()).done(() => {
-		$.get("nav.html",data => {
+		$.get("https://dulldesk.github.io/components/nav.html",data => {
 			$.when((() => {
 				$('#navbar').html(data);
 			})()).done(() => {
@@ -26,7 +26,7 @@ $(document).ready(() => {
 	$.when((() => {
 		$("body").append($("<footer id='footer'></footer>") )
 	})()).done(() => {
-		$.get("footer.html",data => {
+		$.get("https://dulldesk.github.io/components/footer.html",data => {
 			$('#footer').html(data);
 		});
 	})
