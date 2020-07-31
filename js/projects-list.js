915a553457dab1648fc5c7587876c0d0c25fb070
dulldesk <54601393+dulldesk@users.noreply.github.com>
2020-07-30 23:38:22 -0400

    Move `work` object to external file
    The `work` object contains data for the projects page

---
+++ js/projects-list.js
@@ -0,0 +1,97 @@
+const work = {
+	"timestamp" : {
+		"name" : "Timestamp Editor",
+		"caption" : "PowerShell script to edit file timestamps",
+		"lang" : "PowerShell",
+		"github" : "https://github.com/dulldesk/timestamp-editor"
+	}, 
+	"quick-notes" : {
+		"name" : "Quick Notes",
+		"caption" : "Browser extension to take short, quick notes",
+		"lang" : "JavaScript, JQuery, HTML, CSS",
+		"github" : "https://github.com/dulldesk/quick-notes"
+	},
+	"website" : {
+		"name" : "Personal Website",
+		"caption" : "My personal portfolio website",
+		"lang" : "HTML, CSS, JavaScript, JQuery",
+		"link" : "https://dulldesk.github.io",
+		"github" : "https://github.com/dulldesk/dulldesk.github.io",
+		"short" : "dulldesk.github.io"
+	},
+	"sched-del" : {
+		"name" : "Scheduled File Deleter",
+		"caption" : "PowerShell script to schedule file deletions",
+		"lang" : "PowerShell",
+		"github" : "https://github.com/dulldesk/sched-file-del"
+	}, 
+	"recycle" : {
+		"name" : "Recycle Bin Size Script",
+		"caption" : "A small script to check one's recycle bin size",
+		"lang" : "PowerShell",
+		"gist" : "https://gist.github.com/dulldesk/4529497dd9f10c8f68d7d4dd4cf9599a"
+	}, 
+	"covid-cashier" : {
+		"name" : "COVID Cashier",
+		"caption" : "A game to inform on safe restaurant cashiering workplace during a pandemic",
+		"lang" : "Java",
+		"github" : "https://github.com/dulldesk/covid-cashier"
+	},
+	"chords" : {
+		"name" : "Chords",
+		"caption" : "A small site with an online piano. Play, save, and share chords.",
+		"lang" : "HTML, CSS, JavaScript, JQuery",
+		"github" : "https://github.com/dulldesk/chords",
+		"link" : "https://dulldesk.github.io/chords",
+		"short" : "dulldesk.github.io/chords",
+	},
+	"user-gen" : {
+		"name" : "Username Generator",
+		"caption" : "Generates a username based on a random adjective and noun.",
+		"lang" : "HTML, CSS, JavaScript, JQuery",
+		"github" : "https://github.com/dulldesk/alliter-gen/",
+		"link" : "https://dulldesk.github.io/alliter-gen/",
+		"short" : "dulldesk.github.io/alliter-gen/",
+		"ongoing" : true
+	},
+	"words-api" : {
+		"name" : "Words API",
+		"caption" : "An API that returns a noun, adjective, or animal via a GET request to the site.",
+		"lang" : "JavaScript: Node.js, Express.js",
+		"github" : "https://github.com/dulldesk/words-api/",
+		"link" : "https://random-word-form.herokuapp.com/",
+		"short" : "random-word-form.herokuapp.com",
+		"ongoing" : true
+	},
+	"calendar" : {
+		"name" : "Calendar Phone Background Generator",
+		"caption" : "A Processing application to generate a calendar background for a smartphone",
+		"lang" : "Processing",
+		"github" : "https://github.com/dulldesk/calendar",
+		"ongoing" : true
+	},
+	"drivetest" : {
+		"name" : "DriveTest Centre Checker",
+		"caption" : "A script to check whether DriveTest centres have opened (closure is due to the COVID-19 pandemic)",
+		"lang" : "JavaScript: Node.js, Nightmare.js",
+		"github" : "https://github.com/dulldesk/drivetest-check"
+	},
+	"quicksort" : {
+		"name" : "Quicksort Demo",
+		"caption" : "A Processing to demonstrate the quicksort algorithm",
+		"lang" : "Processing",
+		"github" : "https://github.com/dulldesk/quicksort-demo"
+	},
+	"twitter" : {
+		"name" : "TTC Twitter Bot",
+		"caption" : "A Twitter bot to filter and private message subscribing users TTC service tweets relevant to their routes",
+		"lang" : "JavaScript",
+		"ongoing" : true
+	},
+	"collage" : {
+		"name" : "Collage Generator",
+		"caption" : "A collage generator site. Upload an image and a collage will be created. Inspired by an AP Computer Science Student Lab.",
+		"lang" : "HTML, CSS, JavaScript",
+		"ongoing" : true
+	}
+}
