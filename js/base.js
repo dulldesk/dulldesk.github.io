6102ea8249b2ccd75226f2aecdb0ae64477f37eb
dulldesk <54601393+dulldesk@users.noreply.github.com>
2020-08-11 14:25:34 -0400

    Only inject the navbar and the footer if they do not exist
    "Just in case"

--- js/base.js
+++ js/base.js
@@ -1,35 +1,37 @@
 $(document).ready(() => {
-	$.when((() => {
+	if (!$("nav").html()) {
+		$.when((() => {
+			$("body").prepend($("<nav id='navbar'></nav>") )
+		})()).done(() => {
+			$.get("/components/nav.html",data => {
+				$.when((() => {
+					$('#navbar').html(data);
+				})()).done(() => {
+					$('.menuham').click(() => {
+						// $('.menubar').is(':visible') ? $('.menubar').slideUp() : $('.menubar').slideDown();
+						slideToggle('.menubar');
+					});
 
-		$("body").prepend($("<nav id='navbar'></nav>") )
-	})()).done(() => {
-		$.get("/components/nav.html",data => {
-			$.when((() => {
-				$('#navbar').html(data);
-			})()).done(() => {
-				$('.menuham').click(() => {
-					// $('.menubar').is(':visible') ? $('.menubar').slideUp() : $('.menubar').slideDown();
-					slideToggle('.menubar');
-				});
-
-				$(window).resize(() => {
-					if ($(window).width() > 550) {
-						$('.menubar').show();
-						$('.menubar').css('display','flex');
-					} else $('.menubar').hide();
+					$(window).resize(() => {
+						if ($(window).width() > 550) {
+							$('.menubar').show();
+							$('.menubar').css('display','flex');
+						} else $('.menubar').hide();
+					});
 				});
 			});
 		});
-
-	});
-
-	$.when((() => {
-		$("body").append($("<footer id='footer'></footer>") )
-	})()).done(() => {
-		$.get("/components/footer.html",data => {
-			$('#footer').html(data);
-		});
-	})
+	}
+	
+	if (!$("footer").html()) {
+		$.when((() => {
+			$("body").append($("<footer id='footer'></footer>") )
+		})()).done(() => {
+			$.get("/components/footer.html",data => {
+				$('#footer').html(data);
+			});
+		});		
+	}
 });
 
 function slideToggle(id) {
